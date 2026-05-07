324bc734d35dc6d590679ef37dc3c3a067d02279
Chirag Jain <130098617+jchirag123@users.noreply.github.com>
2026-05-07 13:23:18 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -2,13 +2,17 @@ import smtplib
 from email.message import EmailMessage
 import os
 
-EMAIL = os.environ['Email']
-PASSWORD = os.environ['Email_Password']
+EMAIL = os.environ['EMAIL']
+PASSWORD = os.environ['EMAIL_PASSWORD']
+
+recipients = [
+    "Chirag.Jain1@ltts.com"
+]
 
 msg = EmailMessage()
 msg['Subject'] = 'Github Action Email Test'
 msg['From'] = EMAIL
-msg['To'] = EMAIL
+msg['To'] = ", ".join(recipients)
 
 msg.set_content('Hello! Email sent from GitHub Actions.')
 
